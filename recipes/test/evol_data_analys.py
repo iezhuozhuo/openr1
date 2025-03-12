019a3e94f73cc13941bc2e9c3d22df8c614ece79
ZhuoZhuoBye <iezhuo17@gmail.com>
2025-03-12 14:52:12 +0800

    # add analysis

--- recipes/test/evol_data_analys.py
+++ recipes/test/evol_data_analys.py
@@ -30,103 +30,116 @@ def calculate_fitness(solutions, gt_ans, tokenizer):
     return rwd, details_info
 
 
-tokenizer = AutoTokenizer.from_pretrained("/extrahome0/HF_models/Qwen2.5-7B-Instruct")
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("/extrahome0/HF_models/Qwen2.5-7B-Instruct")
 
-data_path = "/extrahome0/Zhuo/output/evol_s1k/v1/evol_s1k.pkl"
-# data_path = "/extrahome0/Zhuo/output/evol_s1k/v1/evol_s1k_-.pkl"
-with open(data_path, "rb") as file:
-    dps = pickle.load(file)
-for dp in dps:
-    all_solutions = dp["all_solutions"]
-    new_all_solutions = []
-    for solution in all_solutions:
-        if len(solution.split("#")) > 1:
-            # 删除多余的阐述语句
-            solution = "#".join(solution.split("#")[1:-1]).replace("Refined", "")
-        new_all_solutions.append(solution)
-    dp["all_solutions"] = new_all_solutions
+    # data_path = "/extrahome0/Zhuo/output/evol_s1k/v1/evol_s1k.pkl"
+    # data_path = "/extrahome0/Zhuo/output/evol_s1k/v1/evol_s1k_-.pkl"
+    data_path = "/extrahome0/Zhuo/output/evol_s1k/v1/evol_s1k_m.pkl"
+    cnt = 0
+    with open(data_path, "rb") as file:
+        dps = pickle.load(file)
+    for dp in dps:
+        all_solutions = dp["all_solutions"]
+        new_all_solutions = []
+        for solution in all_solutions:
+            if len(solution.split("#")) > 1:
+                # 删除多余的阐述语句
+                solution = "#".join(solution.split("#")[1:-1]).replace("Refined", "")
+            new_all_solutions.append(solution)
+        dp["all_solutions"] = new_all_solutions
 
-w2r, w2w, r2w = 0, 0, 0
-init_r_ratio = []
-evol_r_ratio = []
+    w2r, w2w, r2w = 0, 0, 0
+    init_r_ratio = []
+    evol_r_ratio = []
 
-real_evol = 0
-can_evol = 0
-evol_fitness = []
-init_fit = []
-evol_fit = []
+    real_evol = 0
+    can_evol = 0
+    evol_fitness = []
+    init_fit = []
+    evol_fit = []
 
-sel_evol = 0
+    sel_evol = 0
+    best_right = 0
+    best_rwds = []
 
-for dp in dps:
-    all_solutions = dp["all_solutions"]
-    init_solutions = all_solutions[0:-6]
-    evol_solutions = all_solutions[-6:]
-    gt_answer = extract_answer(dp['attempt'])
+    for dp in dps:
+        all_solutions = dp["all_solutions"]
+        init_solutions = all_solutions[0:-6]
+        evol_solutions = all_solutions[-6:]
+        gt_answer = extract_answer(dp['attempt'])
 
-    init_rwd, init_details = calculate_fitness(init_solutions, gt_answer, tokenizer)
-    init_right = False
-    init_r_cnt = 0
-    for init_info in init_details:
-        if init_info["acc_rwd"] >= 0.5:
-            init_r_cnt += 1
-            init_right = True
+        init_rwd, init_details = calculate_fitness(init_solutions, gt_answer, tokenizer)
+        init_right = False
+        init_r_cnt = 0
+        for init_info in init_details:
+            if init_info["acc_rwd"] >= 0.5:
+                init_r_cnt += 1
+                init_right = True
 
-    evol_rwd, evol_details = calculate_fitness(evol_solutions, gt_answer, tokenizer)
-    evol_right = False
-    evol_r_cnt = 0
-    for evol_info in evol_details:
-        if evol_info["acc_rwd"] >= 0.5:
-            evol_r_cnt += 1
-            evol_right = True
+        evol_rwd, evol_details = calculate_fitness(evol_solutions, gt_answer, tokenizer)
+        evol_right = False
+        evol_r_cnt = 0
+        for evol_info in evol_details:
+            if evol_info["acc_rwd"] >= 0.5:
+                evol_r_cnt += 1
+                evol_right = True
 
-    # 进化有多少的样本从不正确到正确
-    if not init_right and evol_right:
-        w2r += 1
-    if init_right and not evol_right:
-        r2w += 1
-    if not init_right and not evol_right:
-        w2w += 1
+        # 进化有多少的样本从不正确到正确
+        if not init_right and evol_right:
+            w2r += 1
+        if init_right and not evol_right:
+            r2w += 1
+        if not init_right and not evol_right:
+            w2w += 1
 
-    # 进化前后的正确率算不算w2w
-    # if not init_right and not evol_right:
-    #     continue  # 没有进化出来正确答案
-    # 进化前后的正确率
-    init_r_ratio.append(init_r_cnt / len(init_solutions))
-    evol_r_ratio.append(evol_r_cnt / len(evol_solutions))
+        # 进化前后的正确率算不算w2w
+        if not init_right and not evol_right:
+            continue  # 没有进化出来正确答案
+        # 进化前后的正确率
+        init_r_ratio.append(init_r_cnt / len(init_solutions))
+        evol_r_ratio.append(evol_r_cnt / len(evol_solutions))
 
-    # 进化有多少的样本的fitness提升了
-    init_fitness_vals = np.array(init_rwd)
-    evol_fitness_vals = np.array(evol_rwd)
-    # 找到当前代的最优解
-    max_fitness_idx = np.argmax(init_fitness_vals)
-    min_fitness_idx = np.argmin(evol_fitness_vals)
+        # 进化有多少的样本的fitness提升了
+        init_fitness_vals = np.array(init_rwd)
+        evol_fitness_vals = np.array(evol_rwd)
+        # 找到当前代的最优解
+        max_fitness_idx = np.argmax(init_fitness_vals)
+        min_fitness_idx = np.argmin(evol_fitness_vals)
 
-    if init_fitness_vals[max_fitness_idx] < evol_fitness_vals[min_fitness_idx]:
-        real_evol += 1
-    if np.mean(evol_rwd) > np.mean(init_rwd):
-        can_evol += 1
-    # evol_fitness.append(evol_fitness_vals[min_fitness_idx]-init_fitness_vals[max_fitness_idx])
-    evol_fitness.append(np.mean(evol_rwd)-np.mean(init_rwd))
-    init_fit.append(np.mean(init_rwd))
-    evol_fit.append(np.mean(evol_rwd))
+        if init_fitness_vals[max_fitness_idx] < evol_fitness_vals[min_fitness_idx]:
+            real_evol += 1
+        if np.mean(evol_rwd) > np.mean(init_rwd):
+            can_evol += 1
+        # evol_fitness.append(evol_fitness_vals[min_fitness_idx]-init_fitness_vals[max_fitness_idx])
+        evol_fitness.append(np.mean(evol_rwd)-np.mean(init_rwd))
+        init_fit.append(np.mean(init_rwd))
+        evol_fit.append(np.mean(evol_rwd))
 
-    # 多少最优样本选择进化的
-    fitness_vals = np.array(init_rwd + evol_rwd)
-    max_fit_idx = np.argmax(fitness_vals)
-    if max_fit_idx >= len(all_solutions) - 6:
-        sel_evol += 1
+        # 多少最优样本选择进化的
+        fitness_vals = np.array(init_rwd + evol_rwd)
+        max_fit_idx = np.argmax(fitness_vals)
+        if max_fit_idx >= len(all_solutions) - 6:
+            sel_evol += 1
 
-    # TODO 进化之后多样性变化
+        # 最优solution的正确性
+        all_rwd, all_details = calculate_fitness(all_solutions, gt_answer, tokenizer)
+        best_rwd = all_rwd[max_fit_idx]
+        best_acc = all_details[max_fit_idx]['acc_rwd']
+        if best_acc >= 0.5:
+            best_right += 1
+        best_rwds.append(best_rwd)
+        # TODO 进化之后多样性变化
 
 
-print(f"总数据量: {len(dps)}")
-print(f"将错误进化到正确: {w2r/len(dps):.3f}, "
-      f"将正确进化到错误: {r2w/len(dps):.3f}, "
-      f"无法进化出正确的: {w2w/len(dps):.3f}")
-print(f"原始正确率: {np.mean(init_r_ratio):.3f}, 正确率中位数: {np.median(init_r_ratio):.3f}\n"
-      f"进化正确率: {np.mean(evol_r_ratio):.3f}, 正确率中位数: {np.median(evol_r_ratio):.3f}")
-print(f"原始fitness: {np.mean(init_fit):.3f}, 进化fitness: {np.mean(evol_fit):.3f}\n"
-      f"完全提升fitness率: {real_evol}/{len(dps)}, 能够提升fitness率: {can_evol/len(dps):.3f}, "
-      f"平均提升fitness: {np.mean(evol_fitness):.3f}")
-print(f"选择进化的回复率: {sel_evol/len(dps):.3f}")
+    print(f"总数据量: {len(dps)}")
+    print(f"将错误进化到正确: {w2r/len(dps):.3f}, "
+          f"将正确进化到错误: {r2w/len(dps):.3f}, "
+          f"无法进化出正确的: {w2w/len(dps):.3f}")
+    print(f"原始正确率: {np.mean(init_r_ratio):.3f}, 正确率中位数: {np.median(init_r_ratio):.3f}\n"
+          f"进化正确率: {np.mean(evol_r_ratio):.3f}, 正确率中位数: {np.median(evol_r_ratio):.3f}")
+    print(f"原始fitness: {np.mean(init_fit):.3f}, 进化fitness: {np.mean(evol_fit):.3f}\n"
+          f"完全提升fitness率: {real_evol}/{len(dps)}, 能够提升fitness率: {can_evol/len(dps):.3f}, "
+          f"平均提升fitness: {np.mean(evol_fitness):.3f}")
+    print(f"选择进化的回复率: {sel_evol/len(dps):.3f}")
+    print(f"最优回复的正确率: {best_right/len(dps):.3f}, 最优回复的rwd: {np.mean(best_rwds):.3f}")
